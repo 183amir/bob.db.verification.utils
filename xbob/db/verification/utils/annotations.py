8473ea9092bae36fef15cd959c78f3ef055c19e0
Manuel Guenther <manuel.guenther@idiap.ch>
2013-11-13 14:35:12 +0100

    Better way to open files.

--- xbob/db/verification/utils/annotations.py
+++ xbob/db/verification/utils/annotations.py
@@ -37,45 +37,46 @@ def read_annotation_file(file_name, annotation_type):
 
   if not os.path.exists(file_name):
     raise IOError("The annotation file '%s' was not found"%file_name)
-  f = open(file_name, 'r')
 
   annotations = {}
 
-  if str(annotation_type) == 'eyecenter':
-    # only the eye positions are written, all are in the first row
-    line = f.readline()
-    positions = line.split()
-    assert len(positions) == 4
-    annotations['reye'] = (float(positions[1]),float(positions[0]))
-    annotations['leye'] = (float(positions[3]),float(positions[2]))
+  with open(file_name, 'r') as f:
 
-  elif str(annotation_type) == 'named':
-    # multiple lines, no header line, each line contains annotation and position
-    for line in f:
+    if str(annotation_type) == 'eyecenter':
+      # only the eye positions are written, all are in the first row
+      line = f.readline()
       positions = line.split()
-      assert len(positions) == 3
-      annotations[positions[0]] = (float(positions[2]),float(positions[1]))
+      assert len(positions) == 4
+      annotations['reye'] = (float(positions[1]),float(positions[0]))
+      annotations['leye'] = (float(positions[3]),float(positions[2]))
 
-  elif str(annotation_type) == 'idiap':
-    # multiple lines, no header, each line contains an integral keypoint identifier, or other identifier like 'gender', 'age',...
-    for line in f:
-      positions = line.rstrip().split()
-      if positions:
-        if positions[0].isdigit():
-          # position field
-          assert len(positions) == 3
-          id = int(positions[0])
-          annotations['key%d'%id] = (float(positions[2]),float(positions[1]))
-        else:
-          # keyword field
-          assert len(positions) == 2
-          annotations[positions[0]] = positions[1]
+    elif str(annotation_type) == 'named':
+      # multiple lines, no header line, each line contains annotation and position
+      for line in f:
+        positions = line.split()
+        assert len(positions) == 3
+        annotations[positions[0]] = (float(positions[2]),float(positions[1]))
 
-      # finally, we add the eye center coordinates as the center between the eye corners; the annotations 3 and 8 seem to be the pupils...
-      if 'key1' in annotations and 'key5' in annotations:
-        annotations['reye'] = ((annotations['key1'][0] + annotations['key5'][0])/2., (annotations['key1'][1] + annotations['key5'][1])/2.)
-      if 'key6' in annotations and 'key10' in annotations:
-        annotations['leye'] = ((annotations['key6'][0] + annotations['key10'][0])/2., (annotations['key6'][1] + annotations['key10'][1])/2.)
+    elif str(annotation_type) == 'idiap':
+      # multiple lines, no header, each line contains an integral keypoint identifier, or other identifier like 'gender', 'age',...
+      for line in f:
+        positions = line.rstrip().split()
+        if positions:
+          if positions[0].isdigit():
+            # position field
+            assert len(positions) == 3
+            id = int(positions[0])
+            annotations['key%d'%id] = (float(positions[2]),float(positions[1]))
+          else:
+            # keyword field
+            assert len(positions) == 2
+            annotations[positions[0]] = positions[1]
+
+        # finally, we add the eye center coordinates as the center between the eye corners; the annotations 3 and 8 seem to be the pupils...
+        if 'key1' in annotations and 'key5' in annotations:
+          annotations['reye'] = ((annotations['key1'][0] + annotations['key5'][0])/2., (annotations['key1'][1] + annotations['key5'][1])/2.)
+        if 'key6' in annotations and 'key10' in annotations:
+          annotations['leye'] = ((annotations['key6'][0] + annotations['key10'][0])/2., (annotations['key6'][1] + annotations['key10'][1])/2.)
 
   if 'leye' in annotations and 'reye' in annotations and annotations['leye'][1] < annotations['reye'][1]:
     logger.warn("The eye annotations in file '%s' might be exchanged!" % file_name)
