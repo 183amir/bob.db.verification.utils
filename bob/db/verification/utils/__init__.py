375994fc6cc5ab7b16ae506a2a1e8eaae64fcc06
Manuel Guenther <manuel.guenther@idiap.ch>
2014-08-25 20:53:15 +0200

    Added get_config().

--- bob/db/verification/utils/__init__.py
+++ bob/db/verification/utils/__init__.py
@@ -21,3 +21,21 @@ from .database import Database, SQLiteDatabase, ZTDatabase
 from .file import File
 from .annotations import read_annotation_file
 
+def get_config():
+  """Returns a string containing the configuration information.
+  """
+
+  import pkg_resources
+
+  packages = pkg_resources.require(__name__)
+  this = packages[0]
+  deps = packages[1:]
+
+  retval =  "%s: %s (%s)\n" % (this.key, this.version, this.location)
+  retval += "  - python dependencies:\n"
+  for d in deps: retval += "    - %s: %s (%s)\n" % (d.key, d.version, d.location)
+
+  return retval.strip()
+
+# gets sphinx autodoc done right - don't remove it
+__all__ = [_ for _ in dir() if not _.startswith('_')]
